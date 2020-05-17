04b228d511cc426604942a2d784e89fa47fd9c32
xman40100 <andres.pelaez00@hotmail.com>
2020-05-17 18:31:12 -0500

    Added configuration and config reloading, scriptside detection via keywords.

--- README.md
+++ README.md
@@ -8,9 +8,11 @@ lua-mtavscode is an extension for Visual Studio Code editor to add IntelliSense 
 
 lua-mtavscode comes with autocomplete, hover and signature providers for various symbols that belong to MTA San Andreas. The extension can be customized to differentitate between client, server and sharedside symbols, allowing you to see the only the ones that are available for the scriptside you are dealing with. This setting can be customized, allowing special keywords to be used to differentiate both scriptsides, by default, it uses the c_ and s_ syntaxes, as well as looking for "server" and "client" words in the file.
 
+Please note, this is not a full LSP, which means, it doesn't have detection for variables, custom function in the IntelliSense, and other general features of a LSP.
+
 Every autocomplete and hover item describes what does the method do, its parameters, as well as a link to the [MTA Wiki](https://wiki.multitheftauto.com) for full reference!
 
-For the meta.xml file, lua-mtavscode also provides snippets for autocompletion.
+For the meta.xml file, lua-mtavscode also provides snippets for autocompletion, with a little description.
 
 ### Lua OOP
 
@@ -20,9 +22,9 @@ lua-mtavscode provides the signature for both the standard and the OOP ways to u
 
 This extension provides useful snippets for coding your resource, for example, create a command handler, add a custom event and more!
 
-### Project scaffolding
+### Project scaffolding (TO-DO)
 
-The vs-code extension adds the ability to scaffold a resource from the get go, simply with the press of a click!
+This extension adds the ability to scaffold a resource from the get go, simply with the press of a click!
 
 ## Settings
 
@@ -30,4 +32,5 @@ The extension has the following settings:
 
 * ``lua-mtavscode.serverSideFileKeywords``: This setting allows the extension to scan the filename to determine if it belongs to a serverside script. It's an array of strings, and by default, it has the following: ``_s, server``.
 * ``lua-mtavscode.clientSideFileKeywords``: This setting allows the extension to scan the filename to determine if it belongs to a clientside script. It's an array of strings, and by default, it has the following: ``_c, client``.
-* ``lua-mtavscode.showMethodOOPSignature``: This setting allows the extension to show the OOP signatures for a method. It's a boolean, and by default, its value is ``true``.
+* ``lua-mtavscode.provideOOPCompletion``: This setting toggles the visibility of the OOP signatures for a method. It's a boolean, and by default, its value is ``true``.
+* ``lua-mtavscode.provideStructuredCompletion``: This setting toggles the visibility of the structured signatures for a method. It's a boolean, and by default, its value is ``true``.
--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,19 @@
 import * as vscode from 'vscode';
 import { MTAClass } from './MTAClass';
 import Utils from './Utils';
+import { MTASymbol } from './MTASymbol';
+
+let eventCompletionProvider: vscode.Disposable;
+let structuredCompletionProvider: vscode.Disposable;
+
+let oopStaticCompletionProvider: vscode.Disposable;
+let oopCompletionProvider: vscode.Disposable;
+
+let clientKeywords: Array<string> = [];
+let serverKeywords: Array<string> = [];
+let scriptSide: string = "";
 
 let classes: Array<MTAClass> = [];
-
 /**
  * Method called when the extension has been activated. The extension is only activated when Lua files
  * that have the correct pattern to be used.
@@ -19,21 +29,24 @@ export function activate(context: vscode.ExtensionContext) {
 		"account",
 		"player"
 	];
-
+	
 	// And create the MTAClass object.
 	classList.forEach((cls) => {
 		let mtaClass: MTAClass = new MTAClass(cls);
 		classes.push(mtaClass);
 	});
 
-	// Create the completion items aka the IntelliSense list.
-	let completionItemProviderAll: vscode.Disposable = vscode.languages.registerCompletionItemProvider("lua", {
+	// get the current workspace configuration
+	let config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration();
+
+	// Create the completion items aka the IntelliSense list ONLY for the event symbols.
+	eventCompletionProvider = vscode.languages.registerCompletionItemProvider("lua", {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 			// Create a completionItems list.
 			let completionItems: vscode.CompletionList = new vscode.CompletionList();
 
 			classes.forEach((mtaClass) => {
-				let symbols = Object.entries(mtaClass.symbolList);
+				let symbols = Object.entries(mtaClass.symbolList).filter(([name, symbol]) => symbol.type === "event" && symbol.scriptSide === scriptSide);
 				symbols.forEach(([name, symbol]) => {
 					let completionItem: any = createCompletionItem(symbol, false);
 					if (completionItem) {
@@ -44,88 +57,183 @@ export function activate(context: vscode.ExtensionContext) {
 			return completionItems;
 		}
 	}, "");
-	context.subscriptions.push(completionItemProviderAll);
+	context.subscriptions.push(eventCompletionProvider);
 
-	// Create the completion items aka the IntelliSense list.
-	// let completionItemProviderOOP: vscode.Disposable = vscode.languages.registerCompletionItemProvider("lua", {
-	// 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-	// 		// Create a completionItems list.
-	// 		let completionItems: vscode.CompletionList = new vscode.CompletionList();
+	registerStructuredProviders(context, config);
+	registerOOPProviders(context, config);
 
-	// 		classes.forEach((mtaClass) => {
-	// 			let symbols = Object.entries(mtaClass.symbolList);
-	// 			symbols.forEach(([name, symbol]) => {
-	// 				if (symbol.type === "method" && !symbol.isOOPStatic) {
-	// 					let completionItem: any = createCompletionItem(symbol, true);
-	// 					if (completionItem) {
-	// 						completionItems.items.push(completionItem);
-	// 					}
-	// 				}
-	// 			});
-	// 		});
-	// 		return completionItems;
-	// 	}
-	// }, ":");
+	// watch for configuration changes
+	vscode.workspace.onDidChangeConfiguration(event => {
+		let affected = event.affectsConfiguration("lua-mtavscode");
+		if (affected) {
+			// reload config
+			config = vscode.workspace.getConfiguration();
+			registerStructuredProviders(context, config);
+			registerOOPProviders(context, config);
 
-	// context.subscriptions.push(completionItemProviderOOP);
+			// reload the allowed keywords.
+			loadClientKeywords(config);
+			loadServerKeywords(config);
+		}
+	});
 
-	// // Create the completion items aka the IntelliSense list.
-	// let completionItemProviderOOPStatic: vscode.Disposable = vscode.languages.registerCompletionItemProvider("lua", {
-	// 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-	// 		// Create a completionItems list.
-	// 		let completionItems: vscode.CompletionList = new vscode.CompletionList();
+	loadClientKeywords(config);
+	loadServerKeywords(config);
 
-	// 		classes.forEach((mtaClass) => {
-	// 			let symbols = Object.entries(mtaClass.symbolList);
-	// 			symbols.forEach(([name, symbol]) => {
-	// 				if (symbol.type === "method" && symbol.isOOPStatic) {
-	// 					let completionItem: any = createCompletionItem(symbol, true);
-	// 					if (completionItem) {
-	// 						completionItems.items.push(completionItem);
-	// 					}
-	// 				}
-	// 			});
-	// 		});
-	// 		return completionItems;
-	// 	}
-	// }, ".");
+	// watch for switching files.
+	vscode.window.onDidChangeActiveTextEditor((textEditor) => {
+		if (textEditor) {
+			let document: vscode.TextDocument = textEditor.document;
+			getFileSide(document);
+		}
+	});
 
-	// context.subscriptions.push(completionItemProviderOOPStatic);
+	let textEditor: vscode.TextEditor|undefined = vscode.window.activeTextEditor;
+	if (textEditor) {
+		let document: vscode.TextDocument = textEditor.document;
+		getFileSide(document);
+	}
 }
 
-function createCompletionItem(mtaSymbol: any, oopOnly: boolean = false): vscode.CompletionItem|undefined {
-	let itemKind: vscode.CompletionItemKind = vscode.CompletionItemKind.Method;
-	let className: string = Utils.firstLetterUpper(mtaSymbol.parentClass);
-	let symbolName: string = mtaSymbol.name;
-	if (mtaSymbol.type === "method" && oopOnly) {
-		if (mtaSymbol.isOOPStatic) {
-			if (!mtaSymbol.oopName) {
-				symbolName = className;
-				itemKind = vscode.CompletionItemKind.Constructor;
-			} else {
-				symbolName = mtaSymbol.oopName;
-			}
+function getFileSide(document: vscode.TextDocument): string {
+	if (document && !document.isUntitled) {
+		let filePath: string = document.uri.fsPath;
+		let lastPosition: number = filePath.lastIndexOf("/");
+		let fileName: string = filePath.substr(lastPosition);
+		
+		let clientFile = clientKeywords.some((keyword) => fileName.includes(keyword));
+		let serverFile = serverKeywords.some((keyword) => fileName.includes(keyword));
+
+		if (clientFile && !serverFile) {
+			scriptSide = "client";
+		} else if (!clientFile && serverFile) {
+			scriptSide = "server";
 		} else {
-			symbolName = mtaSymbol.oopName;
+			scriptSide = "shared";
 		}
 	} else {
-		if (mtaSymbol.type === "method") {
-			itemKind = vscode.CompletionItemKind.Method;
-			symbolName = mtaSymbol.name;
-		} else {
-			itemKind = vscode.CompletionItemKind.Event;
-			symbolName = mtaSymbol.name;
+		scriptSide = "shared";
+	}
+	vscode.window.showInformationMessage("Scriptside: " + scriptSide);
+	return scriptSide;
+}
+
+function registerStructuredProviders(context: vscode.ExtensionContext, config: vscode.WorkspaceConfiguration) {
+	if (structuredCompletionProvider) {
+		structuredCompletionProvider.dispose();
+	}
+
+	if (config.get("lua-mtavscode.provideStructuredCompletion")) {
+		// Create the completion items aka the IntelliSense list.
+		structuredCompletionProvider = vscode.languages.registerCompletionItemProvider("lua", {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+				// Create a completionItems list.
+				let completionItems: vscode.CompletionList = new vscode.CompletionList();
+	
+				classes.forEach((mtaClass) => {
+					let symbols = Object.entries(mtaClass.symbolList).filter(([name, symbol]) => symbol.type === "method" && symbol.scriptSide === scriptSide);
+					symbols.forEach(([name, symbol]) => {
+						let completionItem: any = createCompletionItem(symbol, false);
+						if (completionItem) {
+							completionItems.items.push(completionItem);
+						}
+					});
+				});
+				return completionItems;
+			}
+		}, "");
+		context.subscriptions.push(structuredCompletionProvider);
+	}
+}
+
+function registerOOPProviders(context: vscode.ExtensionContext, config: vscode.WorkspaceConfiguration) {
+	if (oopStaticCompletionProvider) {
+		oopStaticCompletionProvider.dispose();
+	}
+
+	if (oopCompletionProvider) {
+		oopCompletionProvider.dispose();
+	}
+
+	if (config.get("lua-mtavscode.provideOOPCompletion")) {
+		// Create the completion items aka the IntelliSense list ONLY for static methods.
+		oopStaticCompletionProvider = vscode.languages.registerCompletionItemProvider("lua", {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+				// Create a completionItems list.
+				let completionItems: vscode.CompletionList = new vscode.CompletionList();
+	
+				classes.forEach((mtaClass) => {
+					let symbols = Object.entries(mtaClass.symbolList).filter(([name, symbol]) => symbol.type === "method" && symbol.isOOPStatic && symbol.scriptSide === scriptSide);
+					symbols.forEach(([name, symbol]) => {
+						let completionItem: any = createCompletionItem(symbol, true);
+						if (completionItem) {
+							completionItems.items.push(completionItem);
+						}
+					});
+				});
+				return completionItems;
+			}
+		}, ".");
+		context.subscriptions.push(oopStaticCompletionProvider);
+
+		// Create the completion items aka the IntelliSense list ONLY for normal OOP methods.
+		oopCompletionProvider = vscode.languages.registerCompletionItemProvider("lua", {
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+				// Create a completionItems list.
+				let completionItems: vscode.CompletionList = new vscode.CompletionList();
+	
+				classes.forEach((mtaClass) => {
+					let symbols = Object.entries(mtaClass.symbolList).filter(([name, symbol]) => symbol.type === "method" && !symbol.isOOPStatic && symbol.scriptSide === scriptSide);
+					symbols.forEach(([name, symbol]) => {
+						let completionItem: any = createCompletionItem(symbol, true);
+						if (completionItem) {
+							completionItems.items.push(completionItem);
+						}
+					});
+				});
+				return completionItems;
+			}
+		}, ":");
+		context.subscriptions.push(oopCompletionProvider);
+	}
+}
+
+function loadClientKeywords(config: vscode.WorkspaceConfiguration): void {
+	clientKeywords = config.get("lua-mtavscode.clientSideFileKeywords") ?? [];
+}
+
+function loadServerKeywords(config: vscode.WorkspaceConfiguration): void {
+	serverKeywords = config.get("lua-mtavscode.serverSideFileKeywords") ?? [];
+}
+
+function createCompletionItem(mtaSymbol: MTASymbol, oopOnly: boolean = false): vscode.CompletionItem|undefined {
+	let itemKind: vscode.CompletionItemKind;
+	let className: string = Utils.firstLetterUpper(mtaSymbol.parentClass);
+	let symbolName: string = mtaSymbol.name;
+	let detailType: string = "method";
+	let insertText: string = mtaSymbol.insertText;
+
+	if (mtaSymbol.type === "method") {
+		itemKind = vscode.CompletionItemKind.Method;
+		if (!mtaSymbol.oopName) {
+			itemKind = vscode.CompletionItemKind.Constructor;
+			detailType = "constructor method";
 		}
+
+		if (oopOnly) {
+			symbolName = mtaSymbol.oopName ?? className;
+			insertText = mtaSymbol.insertTextOOP;
+		}
+
+	} else {
+		itemKind = vscode.CompletionItemKind.Event;
+		detailType = "event";
 	}
 
 	let completionItem: vscode.CompletionItem = new vscode.CompletionItem(symbolName, itemKind);
 	completionItem.documentation = mtaSymbol.mdString;
-	completionItem.insertText = new vscode.SnippetString(mtaSymbol.insertText);
-	completionItem.detail = `${className} class - `;
-	if (!mtaSymbol.oopName && mtaSymbol.type === "method") {
-		completionItem.detail = completionItem.detail + "constructor ";
-	}
-	completionItem.detail = completionItem.detail + "" + mtaSymbol.type;
+	completionItem.insertText = new vscode.SnippetString(insertText);
+	completionItem.detail = `${className} class - ${detailType} - ${mtaSymbol.scriptSide}`;
 
 	if (mtaSymbol.isDeprecated) {
 		let tags: ReadonlyArray<vscode.CompletionItemTag> = [vscode.CompletionItemTag.Deprecated];
